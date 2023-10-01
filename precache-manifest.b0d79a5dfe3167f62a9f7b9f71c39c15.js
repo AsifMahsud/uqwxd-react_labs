c80b014905bf53ba6aa2999fba06b395764ffb81
AsifMahsud <AsifMahsud543@gmail.com>
2023-10-01 06:49:50 -0400

    Updates

---
+++ precache-manifest.b0d79a5dfe3167f62a9f7b9f71c39c15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c52c32f997d4845070c8",
+    "url": "/uqwxd-react_labs/static/css/main.d429344f.chunk.css"
+  },
+  {
+    "revision": "c52c32f997d4845070c8",
+    "url": "/uqwxd-react_labs/static/js/main.6824e2d6.chunk.js"
+  },
+  {
+    "revision": "2e276793e04b5385066f",
+    "url": "/uqwxd-react_labs/static/js/runtime~main.643b345b.js"
+  },
+  {
+    "revision": "ac6deae7d6ec5d803b76",
+    "url": "/uqwxd-react_labs/static/js/2.b64ec573.chunk.js"
+  },
+  {
+    "revision": "09ba65f46ba0e2575607",
+    "url": "/uqwxd-react_labs/static/js/3.57b62fcd.chunk.js"
+  },
+  {
+    "revision": "881011e539fad0934566fa40eea4828f",
+    "url": "/uqwxd-react_labs/index.html"
+  }
+];
